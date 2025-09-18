6b38e2b4c057e23d64d51b97d271ef048e46fddc
permatrag0 <131209687+permatrag0@users.noreply.github.com>
2025-09-17 18:23:49 -0600

    Delete ggstats/ggstats directory

--- ggstats/ggstats/players/pkg/model/model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import model "ggstats.com/metadata/pkg"
-
-type PlayerResults struct {
-	Scorep1  *int           `json:"scorep1,omitempty"`
-	Scorep2  *int           `json:"scorep2,omitempty"`
-	Metadata model.Metadata `json:"metadata"`
-}
